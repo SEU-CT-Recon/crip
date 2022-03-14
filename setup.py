5e64d2de06bd7a1b89f9d7e60dbc0b9ced84c89c
CandleHouse <769316285@qq.com>
2022-03-15 01:22:20 +0800

    coding

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import setuptools
 
 setuptools.setup(name="crip",
-                 version="0.2",
-                 author="z0gSh1u",
+                 version="0.4",
+                 author="CandleHouse",
                  author_email="zx.cs@qq.coom",
                  description="Cone-Beam CT Data IO, Pre/Post-process and Physics",
                  url="https://github.com/z0gSh1u/crip",
