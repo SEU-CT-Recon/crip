edeb315601c5dd1993ad202ba5bc0855ce3c7b4e
CandleHouse <769316285@qq.com>
2022-03-17 23:21:01 +0800

    Coding

--- crip/dect.py
+++ crip/dect.py
@@ -23,20 +23,20 @@ def singleMatMuDecompose(src: Atten, basis1: Atten, basis2: Atten, energyRange=D
     return coef
 
 
-def singleMaterialMuDecomposeRatio(material, rho, material_1, rho_1, material_2, rho_2, energyRange: tuple) -> np.array:
+def singleMaterialMuDecomposeRatio(src: Atten, basis1: Atten, basis2: Atten, energyRange=DiagEnergyRange) -> np.array:
     '''
         Point-wise material decompose.
         All attenuation have the same weight.
     '''
-    # whats this?
+    range_ = np.array(energyRange)
 
-    attenMu1 = getBuiltInAtten(material_1, rho_1).mu[energyRange[0]:energyRange[1]]
-    attenMu2 = getBuiltInAtten(material_2, rho_2).mu[energyRange[0]:energyRange[1]]
-    attenMu = getBuiltInAtten(material, rho).mu[energyRange[0]:energyRange[1]]
+    attenMu = src.mu[range_]
+    attenMu1 = basis1.mu[range_]
+    attenMu2 = basis2.mu[range_]
 
-    vector = np.array([attenMu1, attenMu2], dtype=float) / attenMu
+    M = np.array([attenMu1, attenMu2], dtype=float) / attenMu
     attenMu = attenMu / attenMu
-    coef = attenMu @ vector.T @ np.linalg.inv(vector @ vector.T)
+    coef = np.linalg.pinv(M) @ (attenMu.T)
     return coef
 
 
