3b416b39b92f133a1e228900c4b41fcfd29a46e6
z0gSh1u <136371391@qq.com>
2022-05-16 21:52:15 +0800

    Recover _typing

---
+++ crip/_typing.py
@@ -0,0 +1,31 @@
+'''
+    Internal typing module of crip.
+
+    https://github.com/z0gSh1u/crip
+'''
+
+import numpy as np
+from typing import List, Union, Tuple, Iterable, Dict, Callable
+
+Or = Union
+
+DefaultFloatDType = np.float32
+DefaultEnergyUnit = 'keV'
+BuiltInAttenEnergyUnit = 'MeV'
+DefaultMuUnit = 'mm-1'
+
+UintLike = Or[np.uint8, np.uint16, np.uint32, np.uint64]
+SignedIntLike = Or[int, np.int8, np.int16, np.int32, np.int64]
+IntLike = Or[UintLike, SignedIntLike]
+# @see https://stackoverflow.com/questions/58686018
+try:
+    FloatLike = Or[np.float16, np.float32, np.float64, np.float128, float]
+except:
+    FloatLike = Or[np.float16, np.float32, np.float64, float]
+
+NDArray = np.ndarray
+ListNDArray = List[NDArray]
+
+TwoD = NDArray
+ThreeD = Or[NDArray, ListNDArray]
+TwoOrThreeD = Or[TwoD, ThreeD]
