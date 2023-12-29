2a4c4990f2917f12595dbfa749ede76915ad297f
z0gSh1u <zx.cs@qq.com>
2023-12-30 00:00:39 +0800

    fine

--- crip/plot.py
+++ crip/plot.py
@@ -7,11 +7,15 @@
 import cv2
 import numpy as np
 from matplotlib import font_manager
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from mpl_toolkits.axes_grid1 import ImageGrid
 from ._typing import *
-from .utils import cripAssert, is1D, isInt
-from .physics import Spectrum
+from .utils import cripAssert, is1D, isInt, cripWarning
+from .physics import Spectrum, DiagEnergyRange
+from .shared import resize
 
-__all__ = ['smooth', 'window', 'average', 'addFont', 'fontdict', 'zoomIn', 'plotSpectrum']
+__all__ = ['smooth', 'window', 'average', 'addFont', 'fontdict', 'zoomIn', 'plotSpectrum', 'makeImageGrid']
 
 
 def smooth(data: NDArray, winSize: int = 5):
@@ -90,5 +94,115 @@ def fontdict(family, weight, size):
     return {'family': family, 'weight': weight, 'size': size}
 
 
-def plotSpectrum(spec: Spectrum):
-    pass
+def plotSpectrum(fig, spec: Spectrum):
+    energies = DiagEnergyRange
+    omega = spec.omega
+
+    fig.plot(energies, omega, 'k')
+    fig.xlabel('Energy (keV)')
+    fig.ylabel('Omega')
+
+
+def makeImageGrid(subimages: List[TwoD],
+                  colTitles: List[str],
+                  rowTitles: List[str],
+                  preprocessor=None,
+                  fontdict=None,
+                  crops=None,
+                  cropLocs='bottom right',
+                  cropEdgeColor='yellow',
+                  cropSize=512 // 4,
+                  figsize=None,
+                  vmin0vmax1=True):
+
+    cripWarning(
+        vmin0vmax1,
+        'vmin0vmax1=False is not recommended because it might cause incorrect windowing. Make sure you know what you are doing.'
+    )
+    cripAssert(len(subimages) == ncols * nrows)
+    cripAssert(crops is None or len(crops) == nrows)
+
+    ncols = len(colTitles)
+    nrows = len(rowTitles)
+
+    if isinstance(cropLocs, str):
+        cropLocs = [cropLocs] * nrows
+
+    fig = plt.figure(figsize=figsize or (ncols * 2, nrows * 2))
+
+    if preprocessor:
+        subimages = list(map(lambda ix: preprocessor(*ix), list(enumerate(subimages))))
+
+    grid = ImageGrid(fig, 111, nrows_ncols=(nrows, ncols), axes_pad=0)
+
+    if crops is not None:
+        rects = [
+            patches.Rectangle((x, y), hw, hw, linewidth=1, edgecolor=cropEdgeColor, facecolor='none')
+            for x, y, hw in crops
+        ]
+    else:
+        rects = []
+
+    i = 0
+    for ax, im in zip(grid, subimages):
+        ax.get_yaxis().set_ticks([])
+        ax.get_xaxis().set_ticks([])
+        list(map(lambda x: x.set_visible(False), ax.spines.values()))
+
+        if i % ncols == 0:
+            ax.add_patch(rects[i // ncols])
+
+        patch = zoomIn(im, *crops[i // ncols])
+        patch = resize(patch, (cropSize, cropSize))
+        cropLoc = cropLocs[i // ncols]
+
+        if cropLoc == 'bottom right':
+            im[-patch.shape[0]:, -patch.shape[1]:] = patch
+            patchRect = patches.Rectangle((im.shape[1] - patch.shape[1], im.shape[0] - patch.shape[0]),
+                                          patch.shape[1],
+                                          patch.shape[0],
+                                          linewidth=1,
+                                          edgecolor=cropEdgeColor,
+                                          facecolor='none')
+        elif cropLoc == 'top left':
+            im[:patch.shape[0], :patch.shape[1]] = patch
+            patchRect = patches.Rectangle((0, 0),
+                                          patch.shape[1],
+                                          patch.shape[0],
+                                          linewidth=1,
+                                          edgecolor=cropEdgeColor,
+                                          facecolor='none')
+        elif cropLoc == 'bottom left':
+            im[-patch.shape[0]:, :patch.shape[1]] = patch
+            patchRect = patches.Rectangle((0, im.shape[0] - patch.shape[0]),
+                                          patch.shape[1],
+                                          patch.shape[0],
+                                          linewidth=1,
+                                          edgecolor=cropEdgeColor,
+                                          facecolor='none')
+        elif cropLoc == 'top right':
+            im[:patch.shape[0], -patch.shape[1]:] = patch
+            patchRect = patches.Rectangle((im.shape[1] - patch.shape[1], 0),
+                                          patch.shape[1],
+                                          patch.shape[0],
+                                          linewidth=1,
+                                          edgecolor=cropEdgeColor,
+                                          facecolor='none')
+        else:
+            raise
+
+        ax.add_patch(patchRect)
+
+        if i < len(colTitles):
+            ax.set_title(colTitles[i], loc='center', fontdict=fontdict)
+        i += 1
+
+        if vmin0vmax1:
+            ax.imshow(im, cmap='gray', vmin=0, vmax=1)
+        else:
+            ax.imshow(im, cmap='gray')
+
+    for i in range(nrows):
+        grid[ncols * i].set_ylabel(rowTitles[i], fontdict=fontdict)
+
+    return fig
