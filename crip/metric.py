84276684f48a82c891757fcf979cb337a56eae93
z0gSh1u <zx.cs@qq.com>
2024-02-02 23:27:07 +0800

    AverageMeter

--- crip/metric.py
+++ crip/metric.py
@@ -50,3 +50,24 @@ def calcRMSE(x, y):
     '''
     sq = (x - y)**2
     return np.sqrt(sq.mean())
+
+
+class AverageMeter():
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val):
+        self.val = val
+        self.sum += val
+        self.count += 1
+        self.avg = self.sum / self.count
+
+    def getValue(self):
+        return self.avg
